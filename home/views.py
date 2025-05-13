7585d84b5e44ae17b0271b04503958dd8ebcdcd4
Guilherme <guigadelmonterios@gmail.com>
2025-05-13 20:54:02 -0300

    mapa finalizado

--- home/views.py
+++ home/views.py
@@ -3,31 +3,38 @@ from django.conf import settings
 from django.shortcuts import render
 import pandas as pd
 from geopy.distance import geodesic
-from django.http import HttpResponse
-
+import json
 
 # Create your views here.
 EXCEL_PATH = os.path.join(settings.BASE_DIR, 'home', 'data', 'farmersmarket.xlsx')
 df = pd.read_excel(EXCEL_PATH)
-print("df passou")
 
 def home(request):
     context = {}
+    markets = []
+    user_lat = None
+    user_lon = None
 
     if request.method == "POST":
-        print("aaaaaaaaaaa")
 
         try:
-            EXCEL_PATH = os.path.join(settings.BASE_DIR, 'home', 'data', 'farmersmarket.xlsx')
+
+            EXCEL_PATH = os.path.join(settings.BASE_DIR, 'home', 'data', 'csa_2025-56131011.xlsx')
             df = pd.read_excel(EXCEL_PATH)
             print("df passou")
 
-
+            
             user_lat = float(request.POST.get("latitude"))
             user_lon = float(request.POST.get("longitude"))
             max_distance = float(request.POST.get("max_distance"))
+            food_type=(request.POST.get("food_type"))
             user_location = (user_lat, user_lon)
 
+            #remover coordenadas Nan
+            df = df.dropna(subset=["location_x","location_y"])
+
+            
+            
             # Cálculo de distância
             def calculate_distance(location_x, location_y):
                 try:
@@ -35,21 +42,44 @@ def home(request):
                     return geodesic(user_location, market_location).km
                 except:
                     return None
+            
 
             df["distance_km"] = df.apply(
                 lambda row: calculate_distance(row["location_x"], row["location_y"]),
                 axis=1
             )
+            df = df.dropna(subset=["distance_km"])
+            if food_type == "" :
+                # Filtro por distância escolhida
+                recommended_markets = df[df["distance_km"] <= max_distance].sort_values("distance_km")
+                recommended_markets = recommended_markets.where(pd.notnull(recommended_markets), None)
+                context["markets"] = recommended_markets[
+                    ["listing_name", "location_address", "distance_km","products","location_y","location_x"]
+                ].head(10).values.tolist()
+            
+            else:
+                food_type = food_type.lower()
+                filtrado = df[df["products"].str.lower().str.contains(food_type, na=False)]
+                print(filtrado["products"])
+                recommended_markets=filtrado[filtrado["distance_km"] <= max_distance].sort_values("distance_km")
+                recommended_markets = recommended_markets.where(pd.notnull(recommended_markets), None)
+                context["markets"] = recommended_markets[["listing_name", "location_address", "distance_km","products","location_y","location_x"]].head(10).values.tolist()
+                print(context)
 
-            # Filtro por distância escolhida
-            recommended_markets = df[df["distance_km"] <= max_distance].sort_values("distance_km")
-            context["markets"] = recommended_markets[
-                ["listing_name", "location_address", "distance_km"]
-            ].head(10).values.tolist()
+                
+                
 
         except ValueError:
             context["error"] = "Por favor, insira valores válidos."
 
-    print("aaaa")
+    if user_lat is not None and user_lon is not None:
+        context["latitude"] = user_lat
+        context["longitude"] = user_lon
+
+    if "markets" in context:
+        context["markets_json"] = json.dumps(context["markets"])
+    print(context["markets_json"])
+
+
     return render(request, 'home/home.html',context)
 
